ff357557296acf93fbc584685dd16374323c7970
Tarsum67 <taraum@icloud.com>
2023-11-10 22:53:54 -0600

    added code tp seeds, schema and indexjs

---
+++ Main/db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employment_db;
+CREATE DATABASE employment_db;
+USE employment_db;
+-- Create a "departments" table to store department information
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(16) NOT NULL,
+    last_name VARCHAR(16) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
