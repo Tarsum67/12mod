34c284f83eacd23ca730788b3085c88892ccc477
Tarsum67 <taraum@icloud.com>
2023-11-11 00:27:33 -0600

    finished seedes

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -1,31 +1,23 @@
 INSERT INTO department (name)
-VALUES (''),
-       (''),
-       (''),
-       ('');
+VALUES ('Raid Boss'),
+       ('Strike Boss'),
+       ('Story Boss')
+       ('Prison Boss'); 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('', , ), 
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , ),
-       ('', , );
+VALUES ('Vex',80000 ,2 ), 
+       ('Fallen',500000 , 4),
+       ('Hive',100000 ,3),
+       ('Kabal', 60000,1 ),
+
+
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       (',', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , ),
-       ('', '', , );
+VALUES ('Orxy', 'Taken King',3 ,103),
+       ('Savathun', 'Witch Queen', 3,NULL ),
+       ('Undying', 'Mind', 1, NULL),
+       ('Protheon', 'Modular Mind',1 ,Null ),
+       ('Skolas', 'Kell of Kells',2 ,102 ),
+       ('Kaliks ', 'Reborn',2,NULL),
+       ('Val', 'Ca our',4,104),
+       ('Gahlran', 'Sarrow barror',2 ,Null ),
